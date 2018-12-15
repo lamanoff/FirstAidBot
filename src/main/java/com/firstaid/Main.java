8d89140e524bc6879d47f631e0138f4fee22b71e
Unknown <lamax_99@mail.ru>
2018-12-15 18:05:35 +0500

    Fixes

--- src/main/java/com/firstaid/Main.java
+++ src/main/java/com/firstaid/Main.java
@@ -1,10 +1,9 @@
 package com.firstaid;
-
 public class Main {
 
     public static void main(String args[]) {
-        var dbPath = new Main().getClass().getResource("db.db").toString();
         var dbHandler = new DatabaseHandler();
+        var dbPath = dbHandler.getClass().getResource("db.db").toString();
         dbHandler.connect(dbPath);
         var answerCreator = new AnswerCreator(dbHandler);
         var access_token = System.getenv().get("ACCESS_TOKEN");
