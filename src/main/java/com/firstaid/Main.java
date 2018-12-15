ef7780dae1f3e0bb2d8880c1ca341eeb168a6da2
Unknown <lamax_99@mail.ru>
2018-12-16 00:34:12 +0500

    Revert "+mainloop exception handling"
    This reverts commit fbc20e41ab42bfd34672f4296250def7d439e5d7.

--- src/main/java/com/firstaid/Main.java
+++ src/main/java/com/firstaid/Main.java
@@ -4,16 +4,7 @@ public class Main {
     public static void main(String args[]) {
         var vk_token = System.getenv().get("VK_ACCESS_TOKEN");
         var ai_token = System.getenv().get("AI_ACCESS_TOKEN");
-        while (true) {
-            try {
-                System.out.println("Server started!");
-                var answerCreator = new AnswerCreator(ai_token);
-                var apiHandler = new APIHandler(answerCreator, vk_token);
-            } catch (Exception ex)
-            {
-                System.out.println("Mainloop exception: " + ex.getMessage());
-                System.out.println("Restarting...");
-            }
-        }
+        var answerCreator = new AnswerCreator(ai_token);
+        var apiHandler = new APIHandler(answerCreator, vk_token);
     }
 }
