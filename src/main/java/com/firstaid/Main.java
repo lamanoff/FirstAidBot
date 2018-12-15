7b1a4b283a8108d6db407238a43bccf85ac43508
Unknown <lamax_99@mail.ru>
2018-12-15 18:11:09 +0500

    Revert "Fix"
    This reverts commit 06863e040c9dfc94f5c93092ae5304aab041e0de.

--- src/main/java/com/firstaid/Main.java
+++ src/main/java/com/firstaid/Main.java
@@ -3,8 +3,7 @@ public class Main {
 
     public static void main(String args[]) {
         var dbHandler = new DatabaseHandler();
-        var dbPath = dbHandler.getClass().getResource("db.db").toString();
-        dbHandler.connect(dbPath);
+        dbHandler.connect("db.db");
         var answerCreator = new AnswerCreator(dbHandler);
         var access_token = System.getenv().get("ACCESS_TOKEN");
         var apiHandler = new APIHandler(answerCreator, access_token);
