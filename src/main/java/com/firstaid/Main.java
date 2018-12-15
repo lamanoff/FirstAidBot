06fa21ebd916bfb564eff70541c0699390162785
Unknown <lamax_99@mail.ru>
2018-12-15 18:31:22 +0500

    The second task was implemented
    +vk bot
    +maven
    +deploy

--- src/main/java/com/firstaid/Main.java
+++ src/main/java/com/firstaid/Main.java
@@ -7,7 +7,5 @@ public class Main {
         var answerCreator = new AnswerCreator(dbHandler);
         var access_token = System.getenv().get("ACCESS_TOKEN");
         var apiHandler = new APIHandler(answerCreator, access_token);
-
-        //((com.firstaid.DatabaseHandler) _dbHandler).disconnect();
     }
 }
