06863e040c9dfc94f5c93092ae5304aab041e0de
Unknown <lamax_99@mail.ru>
2018-12-15 18:11:09 +0500

    Fix

--- src/main/java/com/firstaid/Main.java
+++ src/main/java/com/firstaid/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
     public static void main(String args[]) {
         var dbHandler = new DatabaseHandler();
-        dbHandler.connect("db.db");
+        var dbPath = dbHandler.getClass().getResource("db.db").toString();
+        dbHandler.connect(dbPath);
         var answerCreator = new AnswerCreator(dbHandler);
         var access_token = System.getenv().get("ACCESS_TOKEN");
         var apiHandler = new APIHandler(answerCreator, access_token);
